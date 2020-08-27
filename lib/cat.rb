08fe2b1057302933e227e15aef45d79ef2d3ebe7
paytongray18 <paytongray18@gmail.com>
2020-08-27 17:39:35 -0500

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,28 @@
 class Cat
-  # code goes here
+  
+  attr_accessor :owner, :mood
+  attr_reader :breed, :name
+  
+  @@all = []
+
+  def initialize(name,owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+
+  #def new_owner
+
+  
+
 end
+
+#owner = Owner.new("Hermione")
+#cat = Cat.new("Crookshanks", owner)
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,24 @@
 class Dog
-  # code goes here
+  
+  attr_accessor :owner, :mood
+  attr_reader :name, :breed
+
+  @@all = []
+  
+  def initialize(name,owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+
 end
+
+# owner = Owner.new("Timmy")
+# dog = Dog.new("Lassie","Timmy")
